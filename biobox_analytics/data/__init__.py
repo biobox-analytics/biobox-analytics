8fbf3ab7964eb1469ab97396e75554d410e47a3b
Hamza <hamza@biobox.io>
2024-06-19 16:29:26 -0400

    Fix old import

--- biobox_analytics/data/__init__.py
+++ biobox_analytics/data/__init__.py
@@ -1,2 +1,2 @@
 from biobox_analytics.data.adapters.genome import GenomeAdapter
-from biobox_analytics.data.adapters.scrna import ScRNA
+from biobox_analytics.data.adapters.scrna import ScRNAAdapter
